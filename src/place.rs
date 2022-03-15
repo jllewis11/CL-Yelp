002579c5779768d64d50e2927c74fba31166b590
jllewis11 <jllewis11@gmail.com>
2022-03-15 01:00:59 -0700

    Updated fn new for different opt

--- src/place.rs
+++ src/place.rs
@@ -6,19 +6,63 @@ pub struct Place{
     rating: String,
     avgprice: u64,
     comment: Vec<String>,
+    favorites: Vec<String>,
 }
 impl Place{
-    pub fn new(n:String, a:String, c:String, r:String, avg:u64, opt1:Option<String>) -> Place {
-        let mut temp = Place {
-            name: n,
-            address: a,
-            coltype: c,
-            rating: r,
-            avgprice: avg,
-            comment: Vec::new(),
-        };
-        temp.push(opt1.unwrap());
-        return temp;
+    pub fn new(n:String, a:String, c:String, r:String, avg:u64, opt1:Option<String>, opt2:Option<String>) -> Place {
+        //If opt1 and opt2 are None, then comment and favorites are empty vectors when creating an object of Place
+        if opt1.is_none() && opt2.is_none() {
+            let mut temp = Place{
+                name: n,
+                address: a,
+                coltype: c,
+                rating: r,
+                avgprice: avg,
+                comment: Vec::new(),
+                favorites: Vec::new(),
+            };
+            return temp;
+        }
+        //If opt1 is None, then comment is empty vector and opt2 is a string, which is the favorites
+        else if opt1.is_none() {
+            let mut temp = Place{
+                name: n,
+                address: a,
+                coltype: c,
+                rating: r,
+                avgprice: avg,
+                comment: Vec::new(),
+                favorites: opt2.unwrap().split(",").map(|s| s.to_string()).collect(),
+            };
+            return temp;
+        }
+        //If opt2 is None, then favorites is empty vector and opt1 is a string, which is the comment
+        else if opt2.is_none() {
+            let mut temp = Place{
+                name: n,
+                address: a,
+                coltype: c,
+                rating: r,
+                avgprice: avg,
+                comment: opt1.unwrap().split(",").map(|s| s.to_string()).collect(),
+                favorites: Vec::new(),
+            };
+            return temp;
+        }
+        //If opt1 and opt2 are both Some, then comment and favorites are both vectors
+        else {
+            let mut temp = Place{
+                name: n,
+                address: a,
+                coltype: c,
+                rating: r,
+                avgprice: avg,
+                comment: opt1.unwrap().split(",").map(|s| s.to_string()).collect(),
+                favorites: opt2.unwrap().split(",").map(|s| s.to_string()).collect(),
+            };
+            return temp;
+        }
+
     }
     pub fn push(&mut self, s:String) {
         self.comment.push(s);
@@ -32,6 +76,10 @@ impl Place{
         //display a vector of comments
         println!("Comments: ");
         for i in &self.comment {
+            println!("{}", i);
+        }
+        println!("Favorites: ");
+        for i in &self.favorites {
             println!("{}", i);
         }
     }
@@ -59,5 +107,7 @@ impl Place{
     pub fn remove_comment(&mut self, pos:usize) {
         self.comment.remove(pos);
     }
-
+    pub fn remove_favorite(&mut self, pos:usize) {
+        self.favorites.remove(pos);
+    }
 }
